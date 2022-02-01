6873a06bed0cd809cb34490e90f6d96dc5633a96
jjw1270 <jjw1270@naver.com>
2022-02-01 13:08:08 +0900

    22.2.1

---
+++ Voxel_War/Assets/StartServer.cs
@@ -0,0 +1,17 @@
+using Mirage;
+using UnityEngine;
+
+namespace GettingStarted
+{
+    public class StartServer : MonoBehaviour
+    {
+        [SerializeField] private NetworkManager networkManager;
+
+        private void Start()
+        {
+            if (!networkManager) { return; }
+
+            networkManager.Server.StartServer(networkManager.Client);
+        }
+    }
+}
